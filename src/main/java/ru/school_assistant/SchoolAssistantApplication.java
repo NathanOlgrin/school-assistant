f7c528837d86ff148ba335b3e0ee6fc19392507a
NathanOlgrin <matrica_1997@mail.ru>
2024-11-05 14:37:19 +0300

    First commit

---
+++ src/main/java/ru/school_assistant/SchoolAssistantApplication.java
@@ -0,0 +1,13 @@
+package ru.school_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/school_assistant/SchoolAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.school_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
