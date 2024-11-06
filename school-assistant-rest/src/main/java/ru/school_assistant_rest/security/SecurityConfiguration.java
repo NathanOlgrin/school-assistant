215c1dab8524e6104487f3b3d60b9bae9a6bef83
NathanOlgrin <matrica_1997@mail.ru>
2024-11-06 21:15:12 +0300

    Security в резерве - не удалось настроить запросы с page-модуля

--- school-assistant-rest/src/main/java/ru/school_assistant_rest/security/SecurityConfiguration.java
+++ school-assistant-rest/src/main/java/ru/school_assistant_rest/security/SecurityConfiguration.java
@@ -1,29 +1,30 @@
-package ru.school_assistant_rest.security;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-public class SecurityConfiguration {
-
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        return httpSecurity.authorizeHttpRequests(requests -> requests
-                .requestMatchers("/**").hasAuthority("admin"))
-                .formLogin(Customizer.withDefaults())
-                .rememberMe(rememberMe -> rememberMe.key("uniqieAndSecret"))
-                .logout(logout -> logout.deleteCookies("JSESSIONID"))
-                .build();
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-}
+//package ru.school_assistant_rest.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//public class SecurityConfiguration{
+//
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+//        return httpSecurity.authorizeHttpRequests(requests -> requests
+//                        .requestMatchers(HttpMethod.GET).hasAnyAuthority("admin", "user")
+//                        .requestMatchers(HttpMethod.POST).hasAuthority("admin")
+//                        .requestMatchers(HttpMethod.PUT).hasAuthority("admin"))
+//                .rememberMe(rememberMe -> rememberMe.key("uniqieAndSecret"))
+//                .logout(logout -> logout.deleteCookies("JSESSIONID"))
+//                .build();
+//    }
+//
+//    @Bean
+//    PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
+//}
